3a0b7302f0d62f40b1a980613fe8db5f150bf25a
Sagar Maiti <122274356+sagarmaiti26@users.noreply.github.com>
2024-06-27 20:13:52 +0530

    Search Glitch Fixed

--- src/components/Layouts/Menu/Dropdown.jsx
+++ src/components/Layouts/Menu/Dropdown.jsx
@@ -1,33 +1,52 @@
-import MenuItems from './MenuItems';
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import MenuItems from './MenuItems';
 
-
-const Dropdown = ({ submenus, items,dropdown, depthLevel }) => {
+const Dropdown = ({ submenus, items, dropdown, depthLevel }) => {
+  const [open, setOpen] = React.useState(false);
+  const [inputFocused, setInputFocused] = React.useState(false);
+  const navigate = useNavigate();
+  
   depthLevel = depthLevel + 1;
   const dropdownClass = depthLevel > 1 ? 'dropdown-submenu' : '';
-  console.log(submenus);
-  
+
+  const options = items.submenu && Array.isArray(items.submenu) 
+    ? items.submenu.map(item => ({ label: item.label, url: item.url }))
+    : [];
+
   return (
-    <>
-    
-       <ul className={`dropdown ${dropdownClass} ${dropdown ? 'show' : ''}`}>
-       <Autocomplete
-       forcePopupIcon={false}
-      disablePortal
-      id="combo-box-demo"
-      options={items.submenu}
-      className='w-full'
-      renderInput={(params) => <TextField {...params} label={items.label} />}
-    />
-    
-      {submenus.map((submenu, index) => (
+    <ul className={`dropdown ${dropdownClass} ${dropdown ? 'show' : ''}`}>
+      <li onClick={(e) => e.stopPropagation()}>
+        <Autocomplete
+          open={open}
+          onOpen={() => setOpen(true)}
+          onClose={() => setOpen(false)}
+          disablePortal
+          id="combo-box-demo"
+          options={options}
+          className='w-full'
+          renderInput={(params) => (
+            <TextField 
+              {...params} 
+              label={items.label}
+              onFocus={() => setInputFocused(true)}
+              onBlur={() => setInputFocused(false)}
+            />
+          )}
+          onChange={(event, newValue) => {
+            if (newValue && newValue.url) {
+              navigate(newValue.url);
+             
+            }
+          }}
+        />
+      </li>
+      {!inputFocused && submenus.map((submenu, index) => (
         <MenuItems items={submenu} key={index} depthLevel={depthLevel} />
       ))}
     </ul>
-    </>
-   
   );
 };
 
